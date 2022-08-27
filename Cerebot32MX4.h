639aa3537e1fa5478e45379e5f4214d6161e0a7a
ChrisJones79 <ChrisJones79@users.noreply.github.com>
2022-08-27 14:00:45 -0400

    Should be a working set for the servo...

--- Cerebot32MX4.h
+++ Cerebot32MX4.h
@@ -39,7 +39,7 @@
 #pragma config IESO = ON                // Internal/External Switch Over (Enabled)
 #pragma config POSCMOD = HS             // Primary Oscillator Configuration (HS osc mode)
 #pragma config OSCIOFNC = ON            // CLKO Output Signal Active on the OSCO Pin (Enabled)
-#pragma config FPBDIV = DIV_2           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/2)
+#pragma config FPBDIV = DIV_8           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/8)
 #pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Clock Switch Disable, FSCM Disabled)
 #pragma config WDTPS = PS1048576        // Watchdog Timer Postscaler (1:1048576)
 #pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled (SWDTEN Bit Controls))
@@ -49,7 +49,10 @@
 #pragma config ICESEL = ICS_PGx2        // ICE/ICD Comm Channel Select (ICE EMUC2/EMUD2 pins shared with PGC2/PGD2)
 #pragma config PWP = OFF                // Program Flash Write Protect (Disable)
 #pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
-#pragma config CP = OFF    
+#pragma config CP = OFF                 // Code Protect (Protection Disabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
 
 /* ---------------------------------------------------------------
                 On-Board I/O Declarations
--- build/Debug/debug/main.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/debug/main.o: main.c stdtypes.h Cerebot32MX4.h
-
-stdtypes.h:
-
-Cerebot32MX4.h:
